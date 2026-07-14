71fe24050e87cd1c26f5cc424e7d82c2fbabfd04
Youssef <ysf.assis@gmail.com>
2026-07-14 13:21:35 +0200

    [REFACTOR] move Header and Footer into components/layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata, Viewport } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 import "./globals.css";
 
