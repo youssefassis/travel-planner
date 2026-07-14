4708340d8750413447326d982d652f8a9bb04f69
Youssef <ysf.assis@gmail.com>
2026-07-14 15:41:10 +0200

    [STYLE] unify container width across marketing sections and footer

--- src/features/marketing/FeaturedDestinations.tsx
+++ src/features/marketing/FeaturedDestinations.tsx
@@ -104,7 +104,7 @@ export default function FeaturedDestinations() {
 
   return (
     <Section size="md">
-      <Container>
+      <Container size="wide">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
